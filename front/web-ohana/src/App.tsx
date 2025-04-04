0f6a4d1d240d967f4a44f460fa87cc5a29f3c07a
maria99r <maria99r@hotmail.com>
2025-04-04 18:14:39 +0200

    navbar responsivo

--- front/web-ohana/src/App.tsx
+++ front/web-ohana/src/App.tsx
@@ -1,11 +1,14 @@
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-      <p>hola</p>
-    </div>
+      <div className="h-screen relative bg-[url('/src/assets/mountain.jpg')] bg-cover bg-center before:absolute before:inset-0 before:bg-slate-900 before:opacity-50">
+        <div className="z-50"></div>
+      </div>
+    </>
   );
 }
 
--- front/web-ohana/src/components/Navbar.tsx
+++ front/web-ohana/src/components/Navbar.tsx
@@ -1,23 +1,82 @@
+import React, { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RiCloseLargeFill } from "react-icons/ri";
 import classes from "./Navbar.module.css";
 
-function Navbar() {
+const styles = {
+  link: "hover:bg-white hover:text-slate-800 py-1 px-2 rounded-md transition-colors duration-700 block",
+};
+
+export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <div className={classes.nav}>
-      <img
-        src="logo-ohana.webp"
-        alt="Logo Centro Ohana"
-        className={classes.logo}
-      />
-      <ul className={classes.elements}>
-        <li>Servicios</li>
-        <li>Talleres</li>
-        <li>Conócenos</li>
-        <li>Blog</li>
-        <li>Galería</li>
-        <li>Contacto</li>
-      </ul>
-    </div>
+    <nav className={`${classes.navbar} shadow-lg`}>
+      <div className="flex justify-between container items-center mx-auto">
+        <img src="/logo-ohana.webp" alt="Logo" className="w-16" />
+
+        {/* PANTALLA GRANDE */}
+        <ul className="hidden md:flex space-x-4">
+          <li>
+            <a href="/88" className={styles.link}>
+              Servicios
+            </a>
+          </li>
+          <li>
+            <a href="#" className={styles.link}>
+              Talleres
+            </a>
+          </li>
+          <li>
+            <a href="#" className={styles.link}>
+              Blog
+            </a>
+          </li>
+          <li>
+            <a href="#" className={styles.link}>
+              Conócenos
+            </a>
+          </li>
+        </ul>
+        <div className="md:hidden cursor-pointer">
+          {isMenuOpen ? (
+            <RiCloseLargeFill
+              size={20}
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+            />
+          ) : (
+            <GiHamburgerMenu
+              size={20}
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+            />
+          )}
+        </div>
+      </div>
+      {/* MOVIL */}
+      {isMenuOpen && (
+        <ul className="md:hidden flex flex-col text-center space-y-3 mt-4">
+          <li>
+            <a href="/88" className={styles.link}>
+              Servicios
+            </a>
+          </li>
+          <li>
+            <a href="#" className={styles.link}>
+              Talleres
+            </a>
+          </li>
+          <li>
+            <a href="#" className={styles.link}>
+              Blog
+            </a>
+          </li>
+          <li>
+            <a href="#" className={styles.link}>
+              Conócenos
+            </a>
+          </li>
+        </ul>
+      )}
+    </nav>
   );
 }
-
-export default Navbar;
