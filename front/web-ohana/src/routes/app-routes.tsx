ff035bb808ccc85c891b562eb93003899e531c7c
AGALMAD <agalmad@g.educaand.es>
2025-04-21 16:26:20 +0200

    Revert "intentando solucionar fallos commit"
    This reverts commit c3837a8bd2cbb94323d87eff939529bf243b0c47.

--- front/web-ohana/src/routes/app-routes.tsx
+++ front/web-ohana/src/routes/app-routes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-import Login from "../pages/Login";
-import Home from "../pages/Home";
+import Login from "../pages/Login/Login";
+import Home from "../pages/Home/Home";
+import UsersAdmin from "../pages/UsersAdmin/UsersAdmin";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/users-admin" element={<UsersAdmin />} />
       <Route path="*" element={<h1>Página no encontrada</h1>} />
     </Routes>
   );
