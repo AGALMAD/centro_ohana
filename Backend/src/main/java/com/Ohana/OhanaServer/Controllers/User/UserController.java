173f800efbea59a9eaeda1f1999d4f4e295b5d7b
AGALMAD <agalmad@g.educaand.es>
2025-05-16 11:56:32 +0200

    configuración de CORS

--- Backend/src/main/java/com/Ohana/OhanaServer/Controllers/User/UserController.java
+++ Backend/src/main/java/com/Ohana/OhanaServer/Controllers/User/UserController.java
@@ -15,20 +15,17 @@ public class UserController {
 
     private final UserService userService;
 
-    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping
     public ResponseEntity<UserDto> createUser(@RequestBody NewUserRequest newUser) {
         return ResponseEntity.ok(userService.register(newUser));
 
     }
 
-    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping
     public ResponseEntity<List<UserDto>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
-    @CrossOrigin(origins = "http://localhost:5173")
     @PutMapping
     public ResponseEntity<UserDto> updateUser(@RequestBody UpdateUserRequest newData) {
         UserDto userDto = userService.updateUser(newData);
@@ -36,7 +33,6 @@ public class UserController {
         return userDto != null ? ResponseEntity.ok(userDto) : ResponseEntity.notFound().build();
     }
 
-    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping(value = "/me")
     public ResponseEntity<UserDto> getAuthenticatedUser(Principal principal) {
 
@@ -46,7 +42,6 @@ public class UserController {
         return user != null ? ResponseEntity.ok(user) : ResponseEntity.notFound().build();
     }
 
-    @CrossOrigin(origins = "http://localhost:5173")
     @DeleteMapping("/{id}")
     public ResponseEntity<UserDto> deleteUser(@PathVariable String id) {
         UserDto userDto = userService.deleteUser(id);
