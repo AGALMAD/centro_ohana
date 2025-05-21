6db9f11fb7a1df52fe6f0eef6b67d732c201d71b
maria99r <maria99r@hotmail.com>
2025-05-21 22:10:19 +0200

    carrusel trabajadoras

--- front/web-ohana/src/pages/AboutUs.tsx
+++ front/web-ohana/src/pages/AboutUs.tsx
@@ -1,6 +1,17 @@
 import Workers from "../data/workers.json";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 function AboutUs() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
+
   return (
     <>
       <main>
@@ -57,10 +68,70 @@ function AboutUs() {
               </div>
             ))}
           </div>
+
+          {/* tarjetas con trabajadores */}
+          <div className="w-3/4 m-auto">
+            <div className="mt-20">
+              <Slider {...settings}>
+                {data.map((d) => (
+                  <div
+                    key={d.name}
+                    className="bg-white h-[300px] text-black rounded-xl"
+                  >
+                    <div className="h-56 bg-[var(--color-terciary)] flex justify-center items-center rounded-t-xl">
+                      <img
+                        src={d.img}
+                        alt=""
+                        className="h-44 object-cover w-44 rounded-full"
+                      />
+                    </div>
+
+                    <div className="flex flex-col items-center justify-center gap-3 p-2">
+                      <p className="text-xl font-semibold">{d.name}</p>
+                      <p className="text-center">{d.especialidad}</p>
+                    </div>
+                  </div>
+                ))}
+              </Slider>
+            </div>
+          </div>
         </div>
       </main>
     </>
   );
 }
+
+const data = [
+  {
+    name: `Rocío`,
+    img: `/workers/worker01.webp`,
+    especialidad: `Logopeda`,
+  },
+  {
+    name: `Ana`,
+    img: `/workers/worker02.webp`,
+    especialidad: `Psicopedagoga`,
+  },
+  {
+    name: `Sara`,
+    img: `/workers/worker03.webp`,
+    especialidad: `Logopeda`,
+  },
+  {
+    name: `Laura`,
+    img: `/workers/worker04.webp`,
+    especialidad: `Psicóloga`,
+  },
+  {
+    name: `Sonia`,
+    img: `/workers/worker05.webp`,
+    especialidad: `Musicóloga`,
+  },
+  {
+    name: `Lucia`,
+    img: `/workers/worker06.webp`,
+    especialidad: `Logopeda`,
+  },
+];
 
 export default AboutUs;
