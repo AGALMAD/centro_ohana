81013ec8627d9578e4590fac67d0ed13d91a8f58
maria99r <maria99r@hotmail.com>
2025-05-22 13:48:38 +0200

    trabajadoras incluidas

--- front/web-ohana/src/pages/AboutUs.tsx
+++ front/web-ohana/src/pages/AboutUs.tsx
@@ -1,15 +1,43 @@
-import Workers from "../data/workers.json";
+import about from "../data/aboutUs.json";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import workers from "../data/workers.json";
 import Slider from "react-slick";
 
 function AboutUs() {
   const settings = {
+    accessibility: true,
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
@@ -29,7 +57,7 @@ function AboutUs() {
 
           {/* Tarjetas alternadas */}
           <div className="flex flex-col gap-20">
-            {Workers.workers.map((worker, index) => (
+            {about.about.map((worker, index) => (
               <div
                 key={worker.id}
                 className={`flex flex-col md:flex-row ${
@@ -73,10 +101,10 @@ function AboutUs() {
           <div className="w-3/4 m-auto">
             <div className="mt-20">
               <Slider {...settings}>
-                {data.map((d) => (
+                {workers.workers.map((d) => (
                   <div
                     key={d.name}
-                    className="bg-white h-[300px] text-black rounded-xl"
+                    className="bg-white h-[300px] text-[var(--color-primary)] rounded-xl"
                   >
                     <div className="h-56 bg-[var(--color-terciary)] flex justify-center items-center rounded-t-xl">
                       <img
@@ -86,7 +114,7 @@ function AboutUs() {
                       />
                     </div>
 
-                    <div className="flex flex-col items-center justify-center gap-3 p-2">
+                    <div className="flex flex-col items-center justify-center gap-2 p-2">
                       <p className="text-xl font-semibold">{d.name}</p>
                       <p className="text-center">{d.especialidad}</p>
                     </div>
@@ -100,38 +128,5 @@ function AboutUs() {
     </>
   );
 }
-
-const data = [
-  {
-    name: `Rocío`,
-    img: `/workers/worker01.webp`,
-    especialidad: `Logopeda`,
-  },
-  {
-    name: `Ana`,
-    img: `/workers/worker02.webp`,
-    especialidad: `Psicopedagoga`,
-  },
-  {
-    name: `Sara`,
-    img: `/workers/worker03.webp`,
-    especialidad: `Logopeda`,
-  },
-  {
-    name: `Laura`,
-    img: `/workers/worker04.webp`,
-    especialidad: `Psicóloga`,
-  },
-  {
-    name: `Sonia`,
-    img: `/workers/worker05.webp`,
-    especialidad: `Musicóloga`,
-  },
-  {
-    name: `Lucia`,
-    img: `/workers/worker06.webp`,
-    especialidad: `Logopeda`,
-  },
-];
 
 export default AboutUs;
