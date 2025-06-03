99863eb5640d418454f912a85b4811acbb461034
AGALMAD <agalmad@g.educaand.es>
2025-06-03 10:13:44 +0200

    navegación despues de eliminar taller

--- front/web-ohana/src/pages/Activity.tsx
+++ front/web-ohana/src/pages/Activity.tsx
@@ -75,7 +75,7 @@ function ActivityPage() {
         });
 
         setTimeout(() => {
-          navigate("/activities");
+          navigate("/talleres");
         }, 2000);
       } catch (error) {
         console.error("Error al eliminar la actividad:", error);
